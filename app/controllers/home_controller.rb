f72c871a467be6e9fcbb4ac4079502dfb4d5f936
litejun1 <litejun@naver.com>
2015-10-11 09:52:36 +0000

    add calendar

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,11 @@
 class HomeController < ApplicationController
+    @n = 1
   def index
+    unless current_user.nil?
+      @day = Date.new(current_user.created_at.year,current_user.created_at.month,current_user.created_at.day)
+      @n = Date.today
+      @today = (@n - @day).to_i
+    end
   end
   def chart
   end
