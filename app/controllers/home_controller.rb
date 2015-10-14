2dd4aae1723d116e010356c6187ade54e8421b5c
litejun1 <litejun@naver.com>
2015-10-14 14:39:45 +0000

    connect controller

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -14,4 +14,40 @@ class HomeController < ApplicationController
   def test
     
   end
+  
+  def save
+    
+    @a = params[:skill]
+    
+    case @a
+    
+    when "1"
+      data = Development.new
+    when "2"
+      data = Information.new
+    when "3"
+      data = Insight.new
+    when "4"
+      data = Momentum.new
+    when "5"
+      data = Leadership.new
+    when "6"
+      data = Collaboration.new
+    when "7"
+      data = Logistics.new
+    end
+    
+    
+    data.title = params[:title]
+    data.date = params[:date]
+    data.score = params[:score]
+    data.review = params[:review]
+    
+    data.save
+    
+    redirect_to :back
+    
+  end
+  
+  
 end
